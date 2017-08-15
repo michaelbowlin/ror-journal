2e0f128fe0d186a0f9abd0a4c4d85e07bbc5dc68
michaelbowlin <info@michaelbowlin.com>
2017-08-14 20:55:28 -0600

    dev/jr-104: Rendering Partial Collections (#4)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  render plain: {error: 'This is my error message.'}.to_json, status: 422, content_type: 'application/json'
-
 end
+
+# def create
+#   ...
+#       respond_to do |format|
+#         if @reservation.save
+#           format.html do
+#             redirect_to '/'
+#           end
+#           format.json { render json: @reservation.to_json }
+#         else
+#           format.html { render 'new'} ## Specify the format in which you are rendering "new" page
+#           format.json { render json: @reservation.errors } ## You might want to specify a json format as well
+#         end
+#       end
+# end
